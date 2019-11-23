ab0dac52d810eec0981ff25d3c1eecaf17ca293a
Igor Kliushnikov <igor.kliushnikov@hotmail.com>
2019-11-23 20:09:42 +0100

    Make dockerfiles independent from external scripts: committed trivy binary

--- buildDockerImage.sh
+++ buildDockerImage.sh
@@ -5,15 +5,11 @@ do
     case "${option}" in
         a) APP=${OPTARG};;
         i) IMAGE_NAME=${OPTARG};;
-        u) TRIVY_URL=${OPTARG};;
     esac
 done
 
 # exit on any errors (except conditional checks of executed commands)
 set -e
-
-# download and extract Trivy executable
-wget -qO- ${TRIVY_URL} | tar xvzf -
 
 # build kube-scanner docker image
 docker build -t ${IMAGE_NAME} -f dockerfiles/${APP} .
